fc83594a8341af7b307064868309ddd47fe28424
Marina <kulakova309@gmail.com>
2018-10-14 17:12:00 +0300

    Added Views

--- WorshopBase/Controllers/HomeController.cs
+++ WorshopBase/Controllers/HomeController.cs
@@ -4,35 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WorshopBase.Models;
-using WorshopBase.ViewModels;
 
 namespace WorshopBase.Controllers
 {
     public class HomeController : Controller
     {
-        private WorkshopContext _db;
-
-        public HomeController(WorkshopContext db)
-        {
-            _db = db;
-        }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Contact()
+        public ActionResult About()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Error()
+        public ActionResult Contact()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
